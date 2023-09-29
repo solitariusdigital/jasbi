5cbd5fd4f29b44da9e9f8dd48a5eb3a22d151bac
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-09-29 11:39:05 +0330

    update: timeline toggle/logic

--- components/Timeline.js
+++ components/Timeline.js
@@ -6,32 +6,32 @@ import { enToFaDigits } from "@/services/utility";
 
 export default function Timeline({ timelineData }) {
   const [displayDetails, setDisplayDetails] = useState(false);
-  const [details, setDetails] = useState("");
   const [timeline, setTimeline] = useState(timelineData);
-
+  const [details, setDetails] = useState({});
   const [description, setDescription] = useState("");
-
-  useEffect(() => {
-    console.log(timeline);
-  }, [timeline]);
-
-  const selectYear = (index) => {
-    timeline.map((item, i) => {
-      if (index === i) {
-        item.active = true;
-        setDetails(item);
-      } else {
-        item.active = false;
-      }
-    });
-    setDisplayDetails(true);
-  };
 
   const scrollRight = () => {
     document.getElementById("timeline").scrollLeft += 100;
   };
   const scrollLeft = () => {
     document.getElementById("timeline").scrollLeft -= 100;
+  };
+
+  const selectTimeData = (timeIndex, dataIndex) => {
+    timeline.map((item, i) => {
+      if (timeIndex === i) {
+        item.active = true;
+        setDescription(item.data[parseInt(dataIndex)].description);
+        setDetails({
+          year: item.year,
+          title: item.data[parseInt(dataIndex)].title,
+          description: item.data[parseInt(dataIndex)].description,
+        });
+      } else {
+        item.active = false;
+      }
+    });
+    setDisplayDetails(true);
   };
 
   return (
@@ -43,36 +43,65 @@ export default function Timeline({ timelineData }) {
         />
         <div className={classes.timeline} id="timeline">
           {timelineData
-            .map((time, index) => (
-              <div key={index} className={classes.section}>
-                <div className={classes.row}>
-                  <p className={classes.year}>{enToFaDigits(time.year)}</p>
-                  <select
-                    defaultValue={"default"}
-                    onChange={(e) =>
-                      setDescription(time.data[e.target.value].description)
-                    }
-                  >
-                    <option value="default" disabled>
-                      انتخاب
-                    </option>
-                    {time.data.map((item, index) => {
-                      return (
-                        <option key={index} value={index}>
+            .map((time, index) => {
+              const handleSelectChange = (e) => {
+                const selectedIndex = e.target.value;
+                const updatedTimelineData = timelineData.map((t, i) => {
+                  if (i === index) {
+                    return {
+                      ...t,
+                      active: true,
+                    };
+                  } else {
+                    return {
+                      ...t,
+                      active: false,
+                    };
+                  }
+                });
+                setDescription(
+                  updatedTimelineData[index].data[selectedIndex].description
+                );
+                selectTimeData(index, selectedIndex);
+              };
+              return (
+                <div key={index} className={classes.section}>
+                  <div className={classes.row}>
+                    <h3>{enToFaDigits(time.year)}</h3>
+                    <select
+                      value={time.active ? time.selectedIndex : "default"}
+                      onChange={handleSelectChange}
+                    >
+                      {time.data.map((item, i) => (
+                        <option key={i} value={i}>
                           {item.title}
                         </option>
-                      );
-                    })}
-                  </select>
+                      ))}
+                    </select>
+                  </div>
+                  {time.active ? (
+                    <p
+                      onClick={() => selectTimeData(index, 0)}
+                      className={
+                        time.active ? classes.active : classes.description
+                      }
+                    >
+                      {description.slice(0, 25)} ... <span>بیشتر</span>
+                    </p>
+                  ) : (
+                    <p
+                      onClick={() => selectTimeData(index, 0)}
+                      className={
+                        time.active ? classes.active : classes.description
+                      }
+                    >
+                      {time.data[0].description.slice(0, 25)} ...{" "}
+                      <span>بیشتر</span>
+                    </p>
+                  )}
                 </div>
-                <p
-                  onClick={() => selectYear(index)}
-                  className={time.active ? classes.active : classes.title}
-                >
-                  {description.slice(0, 22)} ... <span>بیشتر</span>
-                </p>
-              </div>
-            ))
+              );
+            })
             .reverse()}
         </div>
         <ArrowForwardIosIcon
@@ -82,8 +111,9 @@ export default function Timeline({ timelineData }) {
       </div>
       {displayDetails && (
         <div className={`${classes.details} animate__animated animate__zoomIn`}>
-          <h3>{details.year}</h3>
-          <p>{details.title}</p>
+          <h2>{enToFaDigits(details.year)}</h2>
+          <h3>{details.title}</h3>
+          <p className={classes.description}>{details.description}</p>
         </div>
       )}
     </Fragment>
