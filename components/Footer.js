611b4d7f494fa409a01c649bdda5c2b80c69ca0b
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-09-30 15:32:40 +0330

    update: footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,33 @@
 import classes from "./Footer.module.scss";
 import Router from "next/router";
+import PrecisionManufacturingIcon from "@mui/icons-material/PrecisionManufacturing";
+import { enToFaDigits } from "@/services/utility";
+import Image from "next/legacy/image";
+import logo from "@/assets/logo.svg";
 
 export default function Footer() {
   return (
     <div className={classes.footer}>
       <div className={classes.container}>
-        <div className={classes.information}></div>
+        <div className={classes.logo} onClick={() => Router.push("/")}>
+          <Image width={100} height={100} src={logo} alt="logo" priority />
+        </div>
+      </div>
+      <div className={classes.copyright}>
+        <p>کليه حقوق اين وب اپلیکیشن به دکتر جاسبی تعلق دارد</p>
+        <p>jasbi.com @Copyright {enToFaDigits(1402)}</p>
+        <div
+          className={classes.row}
+          onClick={() =>
+            window.open(
+              "https://docs.google.com/forms/d/e/1FAIpQLSdqKHLBydQIfm06LTtw0wELHaDJJFGU3GNQFsVWNd3t0jz5hA/viewform?usp=sf_link",
+              "_ self"
+            )
+          }
+        >
+          <p className={classes.action}>طراحی، توسعه و پشتیبانی</p>
+          <PrecisionManufacturingIcon sx={{ fontSize: 18 }} />
+        </div>
       </div>
     </div>
   );
